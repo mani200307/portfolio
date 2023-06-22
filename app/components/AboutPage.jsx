e580abc8069da8df5608bb9f79f663217f09d60d
mani200307 <mani89509@gmail.com>
2023-06-22 13:10:07 +0530

    Projects added

--- app/components/AboutPage.jsx
+++ app/components/AboutPage.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 
 const AboutPage = () => {
+
+    const desc = "I'm a full stack web developer, a machine learning enthusiast. I've done several projects in web development";
+
     return (
         <div className='h-full'>
             <div className='mt-5 flex flex-col gap-5 items-center '>
                 <h1 className='text-3xl justify-start font-medium'>About me</h1>
                 <div className='flex mx-5 bg-base-200 rounded-lg shadow-lg p-4'>
-                    <h1 className='text-center'>I'm a full stack web developer, a machine learning enthusiast. I've done several projects in web development</h1>
+                    <h1 className='text-center'>{desc}</h1>
                 </div>
             </div>
         </div>
--- app/components/NamePage.jsx
+++ app/components/NamePage.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 
 const NamePage = () => {
+
+    const name = "Hello, I'm Manikandan";
+    const about = "I'm a full stack web developer and an open source contributor";
+
     return (
         <div className='-mt-10 flex flex-col gap-10 justify-center items-center bg-base-300 h-screen w-screen'>
-            <h1 className='text-3xl font-medium'>Hello, I'm Manikandan</h1>
-            <h1 className='w-80 text-center text-xl'>I'm a full stack web developer and an open source contributor</h1>
+            <h1 className='text-3xl font-medium'>{name}</h1>
+            <h1 className='w-80 text-center text-xl'>{about}</h1>
             <button className='btn'>Projects</button>
         </div>
     )
